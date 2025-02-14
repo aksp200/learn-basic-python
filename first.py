142ce31249a3e3a6c9eaddf2b60d05539ed47fab
Akshay P <8948642+aksp200@users.noreply.github.com>
2025-02-14 23:39:03 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import math
+
+print("hello")
+
+print(math.log(10))
+
+
+#simple comment
+
+""" 
+multiline comment
+
+"""
+
+
+'''
+
+This is another way of multiline comment
+
+'''
+
+
+
